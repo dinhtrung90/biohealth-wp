18ae1515db4ff79406f5b8ce8a7dafb8c53d3560
nhutphan <nhut.phan@adurolife.com>
2021-07-31 23:16:19 +0700

    working on login

---
+++ src/actions/auth.actions.js
@@ -0,0 +1,6 @@
+import { APP_TOKEN, APP_REFRESH_TOKEN } from 'src/constants/constants'
+
+export const clearAuthToken = () => {
+  localStorage.removeItem(APP_TOKEN)
+  localStorage.removeItem(APP_REFRESH_TOKEN)
+}
